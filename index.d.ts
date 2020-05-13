b14efdf82c4bcadd0239b303d30221fc6eb3cb41
Tidyzq <tidyzq@tidyzq.com>
2020-05-13 13:21:45 +0800

    type: add typings

---
+++ index.d.ts
@@ -0,0 +1,72 @@
+export const behavior: WechatMiniprogram.Behavior.Constructor
+
+export interface ComponentId<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends WechatMiniprogram.Component.MethodOption
+> extends String {
+
+}
+
+export function register<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends WechatMiniprogram.Component.MethodOption
+>(
+    options: WechatMiniprogram.Component.Options<TData, TProperty, TMethod> & {
+        id?: string;
+        tagName?: string;
+        template?: string;
+        usingComponents?: Object;
+    },
+): ComponentId<TData, TProperty, TMethod>
+
+export function create<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends WechatMiniprogram.Component.MethodOption
+>(
+    componentId: ComponentId<TData, TProperty, TMethod>,
+    properties?: Partial<WechatMiniprogram.Component.PropertyOptionToData<TProperty>>,
+): Component<TData, TProperty, TMethod>
+export function create(componentId: string, properties?: any): Component<any, any, any>
+
+export interface ComponentJSON {
+    tagName: string;
+    attrs: { name: string; value: any }[];
+    event: { [event: string]: { handler: string; isCapture: boolean; isCatch: boolean; name: string } };
+    children: ComponentJSON[];
+}
+
+export class Component<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends Partial<WechatMiniprogram.Component.MethodOption>
+> {
+    readonly dom: Node;
+    readonly data: Readonly<TData>;
+    readonly instance: WechatMiniprogram.Component.Instance<TData, TProperty, TMethod>;
+
+    dispatchEvent(eventName: string, options?: any): void;
+
+    querySelector(selector: string): Component | undefined;
+
+    querySelectorAll(selector: string): Component[];
+
+    setData(data: Partial<TData> & {[x: string]: any}, callback?: () => void): void;
+
+    triggerLifeTime(lifetime: 'created' | 'ready' | 'attached' | 'moved' | 'detached' | 'saved' | 'restored' | 'error' | 'listenerChanged'): void;
+
+    toJSON(): ComponentJSON[];
+}
+
+export class RootComponent<
+    TData extends WechatMiniprogram.Component.DataOption,
+    TProperty extends WechatMiniprogram.Component.PropertyOption,
+    TMethod extends Partial<WechatMiniprogram.Component.MethodOption>
+> extends Component<TData, TProperty, TMethod> {
+
+    attach(parent: Node): void;
+
+    detach(): void;
+}
